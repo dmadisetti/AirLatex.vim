7d2047d4c692e14db507921adedddcba8f7fa01a
dylan <dylan@catnip.run>
2026-01-02 11:09:22 +0000

    CI Tests Failed checkpoint: 3

--- rplugin/python3/airlatex/lib/range.py
+++ rplugin/python3/airlatex/lib/range.py
@@ -129,10 +129,8 @@ class NaiveAccumulator:
       self.initialize(base)
 
   def initialize(self, array):
-    self.array = [0]
-    self.last_index = 0
-    for i, a in enumerate(array):
-      self.insert(i, a)
+    self.array = [0] + list(array)
+    self.last_index = len(array)
 
   def insert(self, index, value):
     self.array.insert(index, value)
